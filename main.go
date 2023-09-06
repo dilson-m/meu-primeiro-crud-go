d256e7b313d6992fa99264ddc75a947941584a94
dilsonm <dilson.mascarenhas@gmail.com>
2023-09-06 17:45:19 -0300

    Endpoint de leitura de livros

--- main.go
+++ main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 var port = 1337
 
+type Livro struct {
+	Id     int
+	Titulo string
+	Autor  string
+}
+
+var Livros []Livro = []Livro{
+	Livro{
+		Id:     1,
+		Titulo: "O Guarani",
+		Autor:  "Jose de Alencar",
+	},
+	Livro{
+		Id:     2,
+		Titulo: "Cazuza",
+		Autor:  "Viriato Correia",
+	},
+	Livro{
+		Id:     3,
+		Titulo: "Dom Casmurro",
+		Autor:  "Machado de Assis",
+	},
+}
+
 func rotaPrincipal(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "meu primeiro Crud")
+	fmt.Fprint(w, "Bem vindo ao meu primeiro Crud em GoLang\n")
+}
+
+func listarLivros(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	encoder.Encode(Livros)
 }
 
 func configurarRotas() {
+	http.HandleFunc("/Livros", listarLivros)
 	http.HandleFunc("/", rotaPrincipal)
 }
 
 func configurarServidor() {
 	configurarRotas()
 
-	fmt.Print("Servidor esta rodando na porta: ", port)
+	fmt.Printf("Servidor esta rodando na porta: %v \n", port)
 	http.ListenAndServe(":1337", nil) //	DefaultServerMux
 }
 
